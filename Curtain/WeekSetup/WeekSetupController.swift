1f89b7fa319d4b776e651b65e895b12d0ee21dc8
TowaSakurada <towatowa3939@icloud.com>
2025-10-23 19:57:06 +0900

    coredata

--- Curtain/WeekSetup/WeekSetupController.swift
+++ Curtain/WeekSetup/WeekSetupController.swift
@@ -1,34 +1,64 @@
+
+// MARK: environment,fetchrequestはswiftbaseであり、uikitベースではない（class内で使えない）
+
+
+//import SwiftUI
+//import FirebaseDatabase
+//import CoreData
 //
-//  WeekSetupController.swift
-//  Curtain
-//
-//  Created by 櫻田聖和 on 10/21/25.
-//
-
-
-import SwiftUI
-import FirebaseDatabase
-import CoreData
-
-class WeekSetupController {
-    let shared = WeekSetupController()
-    @Environment(\.managedObjectContext) var context
-    @FetchRequest(
-        sortDescriptors: [],
-        animation: .default
-    ) var newItem: FetchedResults<TimerData>
-    
-    
-    private init() {
-
-    }
+//class WeekSetupController {
+//    static let shared = WeekSetupController()
 //    
-//    private func setup() {
-//        // TODO: define observe (rxswift)
+//    
+//    private init() {
+//        
 //    }
-    
-    func sendData(dayTimers: [String]) {
-        newItem.sunday = dayTimers[0]
-    }
-    
-}
+//    //
+//    //    private func setup() {
+//    //        // TODO: define observe (rxswift)
+//    //    }
+//    
+//    func setCoreDataToState(dayTimers: Binding<[String]>) {
+//        for newitem in newItem {
+//            if let sunday = newitem.sunday {
+//                dayTimers[0].wrappedValue = sunday
+//            }
+//            if let monday = newitem.monday {
+//                dayTimers[1].wrappedValue = monday
+//            }
+//            if let tuesday = newitem.tuesday {
+//                dayTimers[2].wrappedValue = tuesday
+//            }
+//            if let wednesday = newitem.wednesday {
+//                dayTimers[3].wrappedValue = wednesday
+//            }
+//            if let thursday = newitem.thursday {
+//                dayTimers[4].wrappedValue = thursday
+//            }
+//            if let friday = newitem.friday {
+//                dayTimers[5].wrappedValue = friday
+//            }
+//            if let saturday = newitem.saturday {
+//                dayTimers[6].wrappedValue = saturday
+//            }
+//        }
+//        
+//    }
+//    
+//    func setDataToCoreData(dayTimers: [String]) async {
+//        let newTimerData = TimerData(context: context)
+//        
+//        context.delete(newItem[0])
+//        
+//        newTimerData.sunday = dayTimers[0]
+//        newTimerData.monday = dayTimers[1]
+//        newTimerData.tuesday = dayTimers[2]
+//        newTimerData.wednesday = dayTimers[3]
+//        newTimerData.thursday = dayTimers[4]
+//        newTimerData.friday = dayTimers[5]
+//        newTimerData.saturday = dayTimers[6]
+//        
+//        try? context.save()
+//    }
+//    
+//}
--- Curtain/WeekSetup/WeekSetupView.swift
+++ Curtain/WeekSetup/WeekSetupView.swift
@@ -7,13 +7,22 @@
 import SwiftUI
 import RxSwift
 import RxRelay
+import CoreData
 
 struct WeekSetupView: View {
     @State var dayTimer:[String] = Array(repeating: "00:00", count: 7)
     @State var modalIndex = 0
     let weekdayList = ["日", "月", "火", "水", "木", "金", "土"]
-    @State private var isPushed:Bool = false
+    @State private var isPushed = false
     @State var isAbleToSendFirebase = false
+    @State private var isPopup = false
+    // let weekSetupController = WeekSetupController.shared
+    
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(
+        sortDescriptors: [],
+        animation: .default
+    ) var newItem: FetchedResults<TimerData>
     var body: some View {
         
             ScrollView {
@@ -23,7 +32,9 @@ struct WeekSetupView: View {
                         Text("変えたら押してください")
                         
                         if isAbleToSendFirebase {
+                            
                             Button(action: {
+                                setDataToCoreData()
                                 // TODO: firebase送信処理をする
                             }, label: {
                                 SendButtonView(buttonColor: Color.blue)
@@ -124,6 +135,11 @@ struct WeekSetupView: View {
                     
                 }
             }
+            .alert("送信完了",isPresented: $isPopup) {
+                
+            } message: {
+                Text("設定が完了しました。")
+            }
             .sheet(isPresented: $isPushed) {
                 WheelView(
                     weekday: weekdayList[modalIndex],
@@ -133,8 +149,61 @@ struct WeekSetupView: View {
                 )
                     .presentationDetents([.medium])
             }
+            .onAppear() {
+                setCoreDataToState()
+            }
             .onDisappear() {
                 isAbleToSendFirebase = false
             }
+    }
+    
+    // MARK: set stored data to dayTimer
+    func setCoreDataToState() {
+        if let newitem = newItem.first {
+            if let sunday = newitem.sunday {
+                dayTimer[0] = sunday
+            }
+            
+            if let monday = newitem.monday {
+                dayTimer[1] = monday
+            }
+            
+            if let tuesday = newitem.tuesday {
+                dayTimer[2] = tuesday
+            }
+            
+            if let wednesday = newitem.wednesday {
+                dayTimer[3] = wednesday
+            }
+            
+            if let thursday = newitem.thursday {
+                dayTimer[4] = thursday
+            }
+            
+            if let friday = newitem.friday {
+                dayTimer[5] = friday
+            }
+            
+            if let saturday = newitem.saturday {
+                dayTimer[6] = saturday
+            }
+        }
+    }
+    
+    func setDataToCoreData() {
+        if let newitem = newItem.first {
+            context.delete(newitem)
+        }
+        
+        let newitem = TimerData(context: context)
+        newitem.sunday = dayTimer[0]
+        newitem.monday = dayTimer[1]
+        newitem.tuesday = dayTimer[2]
+        newitem.wednesday = dayTimer[3]
+        newitem.thursday = dayTimer[4]
+        newitem.friday = dayTimer[5]
+        newitem.saturday = dayTimer[6]
+        
+        try? context.save()
     }
 }
